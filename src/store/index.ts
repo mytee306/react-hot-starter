bc049cead9e3c7a01ea21af726ef230499e86412
mytee306 <mateja176@gmail.com>
2019-04-13 00:51:16 +0200

    Adds redux-logger middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,20 @@
-import { configureStore } from 'redux-starter-kit';
+import logger from 'redux-logger';
+import { configureStore, getDefaultMiddleware } from 'redux-starter-kit';
 import reducer from './reducer';
 
 export default () => {
-  const store = configureStore({ reducer });
+  if (process.env.NODE_ENV === 'production') {
+    return configureStore({ reducer });
+  } else {
+    const store = configureStore({
+      reducer,
+      middleware: [logger, ...getDefaultMiddleware()],
+    });
 
-  if (process.env.NODE_ENV !== 'production') {
     (module as any).hot.accept('./reducer', () =>
       store.replaceReducer(reducer),
     );
-  }
 
-  return store;
+    return store;
+  }
 };
