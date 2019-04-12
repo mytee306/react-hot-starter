8b6c9f38075594a162bc93b85e22192ea916fdb6
mytee306 <mateja176@gmail.com>
2019-04-13 01:22:16 +0200

    Renames state to count

--- src/store/reducer/count.ts
+++ src/store/reducer/count.ts
@@ -6,6 +6,6 @@ export type Count = number;
 export const initialState: Count = 0;
 
 export default createReducer(initialState, {
-  [increment.type]: state => state + 1,
-  [decrementBy.type]: (state, { payload }: DecrementBy) => state - payload,
+  [increment.type]: count => count + 1,
+  [decrementBy.type]: (count, { payload }: DecrementBy) => count - payload,
 });
