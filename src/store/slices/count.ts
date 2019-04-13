375ac1605171d05bc424f8477217bc279f2e9f8b
mytee306 <mateja176@gmail.com>
2019-04-13 17:36:57 +0200

    Removes comments

--- src/store/slices/count.ts
+++ src/store/slices/count.ts
@@ -4,18 +4,17 @@ export type Count = number;
 
 export type IncrementPayload = void;
 
-export type DecrementPayload = Count;
+export type DecrementByPayload = Count;
 
-export type CountPayload = IncrementPayload | DecrementPayload;
+export type CountPayload = IncrementPayload | DecrementByPayload;
 
-// export type CountAction = IncrementAction | DecrementAction; -> void & number
 const slice = createSlice<Count, PayloadAction<CountPayload>>({
   slice: 'count',
   initialState: 0,
   reducers: {
     increment: count => count + 1,
-    // * if you need to use the payload more than once -> action.payload as DecrementPayload;
-    decrementBy: (count, { payload }) => count - (payload as Count),
+    decrementBy: (count, { payload }) =>
+      count - (payload as DecrementByPayload),
   },
 });
 
@@ -23,5 +22,4 @@ export default slice.reducer;
 
 export const {
   actions: { increment, decrementBy },
-  selectors: { getCount }, // * unfavorable because of any state type
 } = slice;
