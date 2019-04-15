2404db45c674d4e9e0ba212e875fd463d7add18e
mateja <mateja176@gmail.com>
2019-04-15 17:47:23 +0200

    Adds Layout with responsive

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -5,47 +5,87 @@ import {
   Theme,
   withStyles,
   WithStyles,
+  IconButton,
+  Hidden,
 } from '@material-ui/core';
+import { unstable_useMediaQuery as useMediaQuery } from '@material-ui/core/useMediaQuery';
+import { ChevronRight } from '@material-ui/icons';
 import React, { SFC, useState } from 'react';
 import Header from '../Header';
 import Nav from '../Nav';
 
+const minWidth = 240;
+
+const margin = 20;
+
 export const LayoutStyle = (theme: Theme) =>
   createStyles({
     drawer: {
-      width: 240,
+      minWidth,
     },
-    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+      minWidth,
+    },
+    toolbar: {
+      ...theme.mixins.toolbar,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'flex-end',
+    },
+    content: {
+      margin,
+      [theme.breakpoints.up('lg')]: {
+        marginLeft: margin + minWidth,
+      },
+      transition: theme.transitions.create(['margin'], {
+        easing: theme.transitions.easing.easeInOut,
+        duration: theme.transitions.duration.standard,
+      }),
+    },
   });
 
-export interface ResponsiveDrawerProps extends WithStyles<typeof LayoutStyle> {}
+export interface LayoutProps extends WithStyles<typeof LayoutStyle> {
+  theme: Theme;
+}
 
-const ResponsiveDrawer: SFC<ResponsiveDrawerProps> = ({
-  classes,
+const Layout: SFC<LayoutProps> = ({
+  classes: { toolbar, drawer, drawerPaper, content },
   children,
+  theme,
 }) => {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
 
   const handleDrawerToggle = () => {
     setOpen(previousOpen => !previousOpen);
   };
 
+  const isLargeScreen = useMediaQuery(theme.breakpoints.up('lg'));
+
   return (
     <section>
-      <Header />
+      <Header toggle={handleDrawerToggle} />
       <Drawer
-        variant="persistent"
+        variant={isLargeScreen ? 'permanent' : 'temporary'}
         open={open}
         onClose={handleDrawerToggle}
-        className={classes.drawer}
+        className={drawer}
+        classes={{
+          paper: drawerPaper,
+        }}
       >
-        <div className={classes.toolbar} />
+        <div className={toolbar}>
+          <Hidden lgUp>
+            <IconButton onClick={handleDrawerToggle}>
+              <ChevronRight />
+            </IconButton>
+          </Hidden>
+        </div>
         <Divider />
         <Nav />
       </Drawer>
-      <main>{children}</main>
+      <main className={content}>{children}</main>
     </section>
   );
 };
 
-export default withStyles(LayoutStyle, { withTheme: true })(ResponsiveDrawer);
+export default withStyles(LayoutStyle, { withTheme: true })(Layout);
