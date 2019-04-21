be183e1f5d85f02931b0c0e7a17df84cb46b0af9
Mateja Petrovic <mateja176@gmail.com>
2019-04-21 12:05:13 +0200

    Adds sass support

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import './index.css';
+import './index.scss';
 import * as serviceWorker from './serviceWorker';
 import configureStore from './store';
 
