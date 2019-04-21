6e91a5e5bb0684e66ef5a4d4d2efb3bdb9212fe5
Mateja Petrovic <mateja176@gmail.com>
2019-04-21 23:13:01 +0200

    Uses react-hot-loader/webpack

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,14 @@
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
+const { produce } = require('immer');
 
 module.exports = function override(config, env) {
   const hotConfig = rewireReactHotLoader(config, env);
-  return hotConfig;
+
+  return produce(hotConfig, newConfig => {
+    newConfig.module.rules.push({
+      test: /\.jsx?$/,
+      include: /node_modules/,
+      use: ['react-hot-loader/webpack'],
+    });
+  });
 };
