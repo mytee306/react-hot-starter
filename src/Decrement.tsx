f41874a0b7754769f4ffd1db359dec6f6c95d068
Mateja Petrovic <mateja176@gmail.com>
2019-04-21 21:55:35 +0200

    Uses formik and yup rto validate decrement form

--- src/Decrement.tsx
+++ src/Decrement.tsx
@@ -1,53 +1,58 @@
 import { Button, Divider, FormControl, TextField } from '@material-ui/core';
-import React, { FC, useState } from 'react';
-import { withState } from 'recompose';
+import { Form, Formik } from 'formik';
+import React, { FC } from 'react';
+import * as yup from 'yup';
 import { Count, CreateDecrementByAction } from './store/slices/count';
+
+export type Values = { amount: Count };
+
+const initialValues: Values = {
+  amount: 1,
+};
+
+export const schema = yup.object().shape({
+  amount: yup
+    .number()
+    .min(-5)
+    .max(5)
+    .required(),
+});
 
 export interface DecrementProps {
   decrementBy: CreateDecrementByAction;
-  amount: Count;
-  setAmount: (amount: Count) => Count;
 }
 
-const Decrement: FC<DecrementProps> = ({ decrementBy, amount, setAmount }) => {
-  const [isErrorDisplayed, displayError] = useState(false);
+const Decrement: FC<DecrementProps> = ({ decrementBy }) => (
+  <Formik
+    initialValues={initialValues}
+    onSubmit={({ amount }) => decrementBy(amount)}
+    validationSchema={schema}
+  >
+    {({ values, errors, handleChange, handleBlur }) => (
+      <Form>
+        <FormControl>
+          <TextField
+            name="amount"
+            label="Decrement By Amount"
+            variant="outlined"
+            type="number"
+            value={values.amount}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={Boolean(errors.amount)}
+            helperText={errors.amount}
+          />
+        </FormControl>
+        <br />
+        <br />
+        <Divider />
+        <br />
+        <Button color="secondary" variant="contained" type="submit">
+          Decrement
+        </Button>
+      </Form>
+    )}
+  </Formik>
+);
 
-  return (
-    <form>
-      <FormControl>
-        <TextField
-          label="Decrement By Amount"
-          variant="outlined"
-          type="number"
-          value={amount}
-          onChange={({ target: { value } }) => {
-            const newAmount = parseInt(value, 10);
-
-            if (Number.isNaN(newAmount)) {
-              displayError(true);
-            } else {
-              setAmount(newAmount);
-
-              displayError(false);
-            }
-          }}
-          error={isErrorDisplayed}
-          helperText={isErrorDisplayed && 'Please input a valid integer'}
-        />
-      </FormControl>
-      <br />
-      <br />
-      <Divider />
-      <br />
-      <Button
-        color="secondary"
-        variant="contained"
-        onClick={() => decrementBy(amount)}
-      >
-        Decrement
-      </Button>
-    </form>
-  );
-};
-
-export default withState('amount', 'setAmount', 1)(Decrement);
+export default Decrement;
