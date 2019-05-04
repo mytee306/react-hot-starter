bd605ca952b1ddd0c57da4b50e9495ad146e770e
mytee306 <mateja176@gmail.com>
2019-05-04 23:45:19 +0200

    Enables nested nav items

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,42 +1,71 @@
-import { List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
+import {
+  Link,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails,
+  withStyles,
+  WithStyles,
+} from '@material-ui/core';
 import {
   ArrowDownward,
   ArrowUpward,
   Dashboard,
   Person,
+  BarChart,
+  ExpandMore,
 } from '@material-ui/icons';
 import dashify from 'dashify';
+import join from 'url-join';
 import React, { SFC } from 'react';
-import { NavLink } from 'react-router-dom';
-
-const Home = 'Dashboard';
-const Increment = 'Increment';
-const Decrement = 'Decrement';
-
-const Login = 'Login';
+import { NavLink, NavLinkProps } from 'react-router-dom';
+import { Omit } from 'utility-types'; // eslint-disable-line import/no-extraneous-dependencies
 
 export interface NavItem {
   text: string;
   icon: JSX.Element;
   path: string;
+  children: NavItem[];
 }
 export type NavItems = NavItem[];
+
+const Login = 'Login';
+
+const Home = 'Dashboard';
+const Count = 'Count';
+const Increment = 'Increment';
+const Decrement = 'Decrement';
 
 export const privateNavItems: NavItems = [
   {
     text: Home,
     icon: <Dashboard />,
     path: '',
+    children: [],
   },
   {
-    text: Increment,
-    icon: <ArrowUpward />,
-    path: dashify(Increment),
-  },
-  {
-    text: Decrement,
-    icon: <ArrowDownward />,
-    path: dashify(Decrement),
+    text: Count,
+    path: dashify(Count),
+    icon: <BarChart />,
+    children: [
+      {
+        text: Increment,
+        icon: <ArrowUpward />,
+        path: dashify(Increment),
+        children: [],
+      },
+      {
+        text: Decrement,
+        icon: <ArrowDownward />,
+        path: dashify(Decrement),
+        children: [],
+      },
+    ],
   },
 ];
 
@@ -45,8 +74,93 @@ const publicNavItems: NavItems = [
     text: Login,
     icon: <Person />,
     path: dashify(Login),
+    children: [],
   },
 ];
+
+interface NavItemProps extends Omit<NavItem, 'children'> {
+  onNavigate: () => void;
+}
+
+const NavItem: SFC<NavItemProps> = ({ onNavigate, text, path, icon }) => (
+  <Link
+    key={text}
+    onClick={onNavigate}
+    component={(props: Omit<NavLinkProps, 'to'>) => (
+      <NavLink {...props} to={path} />
+    )}
+  >
+    <ListItem button>
+      <ListItemIcon>{icon}</ListItemIcon>
+      <ListItemText>{text}</ListItemText>
+    </ListItem>
+  </Link>
+);
+
+interface NestedNavItemProps extends NavItem, WithStyles {
+  onNavigate: () => void;
+}
+
+const summaryStyles = {
+  expanded: {
+    '&$expanded': {
+      marginBottom: 0,
+    },
+  },
+};
+
+const UnstyledNestedNavItem: SFC<NestedNavItemProps> = ({
+  classes,
+  children,
+  onNavigate,
+  ...navItemProps
+}) => (
+  <ExpansionPanel style={{ boxShadow: 'none' }}>
+    <ExpansionPanelSummary
+      expandIcon={<ExpandMore />}
+      classes={{ expanded: classes.expanded }}
+    >
+      <NavItem {...navItemProps} onNavigate={onNavigate} />
+    </ExpansionPanelSummary>
+    <ExpansionPanelDetails>
+      <NavItems navItems={children} onNavigate={onNavigate} />
+    </ExpansionPanelDetails>
+  </ExpansionPanel>
+);
+
+const NestedNavItem = withStyles(summaryStyles)(UnstyledNestedNavItem);
+
+interface NavItemsProps {
+  navItems: NavItems;
+  onNavigate: () => void;
+}
+
+const NavItems: SFC<NavItemsProps> = ({ navItems, onNavigate }) => (
+  <List>
+    {navItems.map(navItem => {
+      const { children, ...withoutChildren } = navItem;
+
+      return children.length ? (
+        <NestedNavItem
+          {...withoutChildren}
+          key={navItem.text}
+          onNavigate={onNavigate}
+        >
+          {children.map(({ path, ...rest }) => ({
+            ...rest,
+            path: join('/', navItem.path, path),
+          }))}
+        </NestedNavItem>
+      ) : (
+        <NavItem
+          {...withoutChildren}
+          key={navItem.text}
+          onNavigate={onNavigate}
+        />
+      );
+    })}
+  </List>
+);
 
 export interface NavProps {
   isLoggedIn: boolean;
@@ -55,16 +169,10 @@ export interface NavProps {
 
 const Nav: SFC<NavProps> = ({ isLoggedIn, onNavigate }) => (
   <nav>
-    <List>
-      {(isLoggedIn ? privateNavItems : publicNavItems).map(({ text, icon, path }) => (
-        <NavLink to={path} key={text} onClick={onNavigate}>
-          <ListItem button key={text}>
-            <ListItemIcon>{icon}</ListItemIcon>
-            <ListItemText>{text}</ListItemText>
-          </ListItem>
-        </NavLink>
-      ))}
-    </List>
+    <NavItems
+      onNavigate={onNavigate}
+      navItems={isLoggedIn ? privateNavItems : publicNavItems}
+    />
   </nav>
 );
 
