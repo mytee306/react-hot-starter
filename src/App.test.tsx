7fb5e548a99014867a655532c33368435f35a0d2
Mateja Petrovic <mateja176@gmail.com>
2019-05-11 18:01:31 +0200

    fix(App.test.tsx)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import configureStore from './store';
+
+const store = configureStore();
+
+window.matchMedia = jest.fn().mockImplementation(query => ({
+  matches: false,
+  media: query,
+  onchange: null,
+  addListener: jest.fn(),
+  removeListener: jest.fn(),
+}));
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>,
+    div,
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
