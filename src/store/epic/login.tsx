4000bee757cb68dd8b0cdb0988c4dbe398f30bbb
Mateja Petrovic <mateja176@gmail.com>
2019-05-27 23:23:07 +0200

    feat: adds barebones login epic

---
+++ src/store/epic/login.tsx
@@ -0,0 +1,26 @@
+import { Epic, ofType } from 'redux-observable';
+import { empty, of } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
+import {
+  createGetAccountAction,
+  createSetAccountAction,
+  createSetAccountErrorAction,
+  initialAccount,
+} from '../slices/account';
+
+const logIn: Epic = action$ =>
+  action$.pipe(
+    ofType(createGetAccountAction.toString()),
+    switchMap(() => empty()),
+    catchError(({ message }) => of(createSetAccountErrorAction(message))),
+  );
+
+// todo listen for logout event
+// todo add logout success and error handling for manual logout
+const logOut: Epic = action$ =>
+  action$.pipe(
+    ofType(createSetAccountAction.toString()),
+    map(() => createSetAccountAction(initialAccount)),
+  );
+
+export default [logIn, logOut];
