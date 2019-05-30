971d5060991367d1b4e807c2454a2ff5f6571b88
Mateja Petrovic <mateja176@gmail.com>
2019-05-30 23:37:41 +0200

    feat(src/store/reducer.ts): expot createReset

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import { createAction } from 'redux-starter-kit';
 import { createSelector, defaultMemoize } from 'reselect';
 import account from './slices/account';
 import count from './slices/count';
@@ -12,12 +13,14 @@ const reducer = combineReducers({
 
 export type State = ReturnType<typeof reducer>;
 
-type Reducer = typeof reducer;
+export type Reducer = typeof reducer;
 
-export const RESET = 'RESET';
+export const createReset = createAction('RESET');
 
 const reducerWithReset: Reducer = (state, action) =>
-  action.type === RESET ? reducer(undefined, action) : reducer(state, action);
+  action.type === createReset.toString()
+    ? reducer(undefined, action)
+    : reducer(state, action);
 
 export default reducerWithReset;
 
