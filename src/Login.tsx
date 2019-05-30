b1a40dc3b1cf326816cff294f41afddd9725d54a
Mateja Petrovic <mateja176@gmail.com>
2019-05-31 00:19:49 +0200

    feat(src/Login.tsx): dispatches login action

--- src/Login.tsx
+++ src/Login.tsx
@@ -6,18 +6,28 @@ import {
   Typography,
 } from '@material-ui/core';
 import React, { FC } from 'react';
+import { connect } from 'react-redux';
+import { CreateSimpleAction } from './models/actions';
+import { createGetAccount } from './store/slices/account';
 
-export interface LoginProps {}
+export interface LoginProps {
+  logIn: CreateSimpleAction;
+}
 
-const Login: FC<LoginProps> = () => (
+const Login: FC<LoginProps> = ({ logIn }) => (
   <Card>
     <CardContent>
       <Typography variant="h2">Welcome</Typography>
     </CardContent>
     <CardActions>
-      <Button variant="contained">Login</Button>
+      <Button variant="contained" onClick={() => logIn()}>
+        Login
+      </Button>
     </CardActions>
   </Card>
 );
 
-export default Login;
+export default connect(
+  null,
+  { logIn: createGetAccount },
+)(Login);
