d0db36a345c0ed0e20d3aec473d1edaa7575ede7
Mateja Petrovic <mateja176@gmail.com>
2019-06-03 01:07:45 +0200

    fix: parses empty object as a firebase config fallback

--- src/firebase/init.ts
+++ src/firebase/init.ts
@@ -4,8 +4,8 @@ import 'firebase/firestore';
 
 const config = process.env.REACT_APP_FIREBASE_CONFIG;
 
-const parsedConfig = JSON.parse(config || '');
+const parsedConfig = JSON.parse(config || '{}');
 
-const app = initializeApp(parsedConfig || {});
+const app = initializeApp(parsedConfig);
 
 export default app;
