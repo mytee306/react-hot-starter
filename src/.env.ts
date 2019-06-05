d386021293e4c4fe762e35499c1969bcfb7b2847
Mateja Petrovic <mateja176@gmail.com>
2019-06-05 23:29:59 +0200

    feat(src/.env.ts): adds env file with all enviroments based on NODE_ENV

---
+++ src/.env.ts
@@ -0,0 +1,32 @@
+const development = {
+  REACT_APP_PUBLIC_URL: 'http://react-hot-starter-dev.firebaseapp.com',
+
+  REACT_APP_FIREBASE_CONFIG: {
+    apiKey: 'AIzaSyDF9z3Z4LO-KP8IqUqV00_2MY-L-YGdbRg',
+    authDomain: 'react-hot-starter-dev.firebaseapp.com',
+    databaseURL: 'https://react-hot-starter-dev.firebaseio.com',
+    projectId: 'react-hot-starter-dev',
+    storageBucket: 'react-hot-starter-dev.appspot.com',
+    messagingSenderId: '456166678835',
+    appId: '1:456166678835:web:a01054c1878394ae',
+  },
+};
+
+export type Env = typeof development;
+
+// TODO remove before deployment
+export type GenericEnv = {
+  [key: string]: string | { [key: string]: string };
+};
+
+export type AllEnvironments = {
+  [envName in typeof process.env.NODE_ENV]: Env | GenericEnv
+};
+
+const allEnvironments: AllEnvironments = {
+  development,
+  production: {},
+  test: {},
+};
+
+export default allEnvironments[process.env.NODE_ENV];
