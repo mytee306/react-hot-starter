c3215556d43a80bf0f95dfe66cd3d2a47bd272e3
Mateja Petrovic <mateja176@gmail.com>
2019-06-14 00:12:57 +0200

    feat(utils): adds mapStateToProps

---
+++ src/utils/index.test.ts
@@ -0,0 +1,13 @@
+import 'polyfill-object.fromentries';
+import { mapStateToProps } from '.';
+import { initialState, selectSignedInFlag } from '../store/reducer';
+
+const mappedState = mapStateToProps({ isSignedIn: selectSignedInFlag })(
+  initialState,
+);
+
+describe('Utilities', () => {
+  test(mapStateToProps.name, () => {
+    expect(mappedState).toEqual({ isSignedIn: false });
+  });
+});
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,26 @@
+/* eslint-disable indent */
+import { Selector } from 'react-redux';
+import { State } from '../store/reducer';
+
 const prefixActionTypeWithSeparator = (separator: string) => (
   prefix: string,
 ) => (actionType: string) => prefix.concat(separator).concat(actionType);
 
 export const prefixActionType = prefixActionTypeWithSeparator('/');
+
+export const mapStateToProps = <
+  Map extends { [key: string]: Selector<State, any> }
+>(
+  map: Map,
+) => (state: State) => {
+  const entries = Object.entries(map).map(([prop, selector]) => [
+    prop,
+    selector(state),
+  ]);
+
+  const fromEntries: {
+    [prop in keyof Map]: ReturnType<Map[prop]>
+  } = Object.fromEntries(entries);
+
+  return fromEntries;
+};
