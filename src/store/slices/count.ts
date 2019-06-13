9590c0fb630bb435e68395a3ad621ec08cf227b7
Mateja Petrovic <mateja176@gmail.com>
2019-06-14 00:37:03 +0200

    feat(slices/count): adds createGetCount

--- src/store/slices/count.ts
+++ src/store/slices/count.ts
@@ -1,21 +1,27 @@
-import { createSlice } from 'redux-starter-kit';
+import { createAction, createSlice } from 'redux-starter-kit';
 import { SliceActionCreator } from 'redux-starter-kit/src/createSlice';
+import { prefixActionType } from '../../utils';
+
+export const slice = 'count';
+
+const prefix = prefixActionType(slice);
 
 export type Count = number;
 
 const initialState: Count = 0;
+
+export const createGetCount = createAction(prefix('get'));
 
 export type CreateDecrementBy = SliceActionCreator<Count>;
 
 export type DecrementByAction = ReturnType<CreateDecrementBy>;
 
 export const {
-  slice,
   reducer,
   actions: { increment: createIncrement, decrementBy: createDecrementBy },
   selectors: { getCount: selectCount },
 } = createSlice({
-  slice: 'count',
+  slice,
   initialState,
   reducers: {
     increment: count => count + 1,
