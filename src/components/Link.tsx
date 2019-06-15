a01915bb592c1fd1f9dca752f5113ae3566ae39b
Mateja Petrovic <mateja176@gmail.com>
2019-06-15 23:39:56 +0200

    feat(components/Link): adds disabled prop

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -3,7 +3,9 @@ import { LinkProps } from '@material-ui/core/Link';
 import React, { FC } from 'react';
 import { NavLink, NavLinkProps } from 'react-router-dom';
 
-const Link: FC<Omit<LinkProps, 'component'> & NavLinkProps> = ({
+const Link: FC<Omit<LinkProps, 'component'> & NavLinkProps & { disabled?: boolean }> = ({
+  disabled,
+  style,
   to,
   color = 'inherit',
   ...props
@@ -11,6 +13,7 @@ const Link: FC<Omit<LinkProps, 'component'> & NavLinkProps> = ({
   <MaterialLink
     {...props}
     color={color}
+    style={{ ...style, pointerEvents: disabled ? 'none' : 'initial' }}
     component={(navLinkProps: Omit<NavLinkProps, 'to'>) => (
       <NavLink {...navLinkProps} to={to} />
     )}
