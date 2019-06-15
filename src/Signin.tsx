8c906077f3d42307108476781c9c6fc53e86c082
Mateja Petrovic <mateja176@gmail.com>
2019-06-15 19:56:50 +0200

    fix(Signin): redirects to previous route if the user is signed in
    if however the user requested to go to /signin explicitly they are taken to the dashboard

--- src/Signin.tsx
+++ src/Signin.tsx
@@ -1,7 +1,7 @@
 import { Card, CardActions, CardContent, Typography } from '@material-ui/core';
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { RouteComponentProps } from 'react-router-dom';
 import Button from './components/Button';
 import {
   selectAuthLoadingFlag,
@@ -10,15 +10,32 @@ import {
 } from './store/reducer';
 import { createSignin, CreateSignin } from './store/slices/auth';
 
-export interface SigninProps {
+export const signinPath = '/signin';
+
+export interface SigninProps extends RouteComponentProps {
   signIn: CreateSignin;
   isAuthLoading: ReturnType<typeof selectAuthLoadingFlag>;
   isSignedIn: ReturnType<typeof selectSignedInFlag>;
 }
 
-const Signin: FC<SigninProps> = ({ signIn, isAuthLoading, isSignedIn }) => (
-  <>
-    {isSignedIn && <Redirect to="/" />}
+const Signin: FC<SigninProps> = ({
+  signIn,
+  isAuthLoading,
+  isSignedIn,
+  history,
+  location: { pathname },
+}) => {
+  useEffect(() => {
+    if (isSignedIn) {
+      if (pathname === signinPath) {
+        history.push('/');
+      } else {
+        history.goBack();
+      }
+    }
+  }, [isSignedIn, pathname, history]);
+
+  return (
     <Card>
       <CardContent>
         <Typography variant="h2">Welcome</Typography>
@@ -33,8 +50,8 @@ const Signin: FC<SigninProps> = ({ signIn, isAuthLoading, isSignedIn }) => (
         </Button>
       </CardActions>
     </Card>
-  </>
-);
+  );
+};
 
 export default connect(
   (state: State) => ({
