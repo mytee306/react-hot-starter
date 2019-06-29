d5c2bef83fa0ff43159fc05f0f000583546475c4
Mateja Petrovic <mateja176@gmail.com>
2019-06-29 19:01:47 +0200

    feat(storybook): adds addons

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,7 +1,18 @@
+import { withA11y } from '@storybook/addon-a11y';
 import { withInfo } from '@storybook/addon-info';
-import { addDecorator, configure } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
+import { addDecorator, addParameters, configure } from '@storybook/react';
 
 addDecorator(withInfo);
+addDecorator(withKnobs);
+addDecorator(withA11y);
+
+addParameters({
+  backgrounds: [
+    { name: 'twitter', value: '#00aced' },
+    { name: 'facebook', value: '#3b5998' },
+  ],
+});
 
 const req = require.context('../stories', true, /\.stories\.tsx$/);
 
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,10 +1,21 @@
 module.exports = ({ config }) => {
   config.module.rules.push({
-    test: /\.tsx?$/,
+    test: /\.tsx$/,
     loader: require.resolve('babel-loader'),
     options: {
       presets: [['react-app', { flow: false, typescript: true }]],
     },
+  });
+
+  config.module.rules.push({
+    test: /\.stories\.tsx$/,
+    loaders: [
+      {
+        loader: require.resolve('@storybook/addon-storysource/loader'),
+        options: { parser: 'typescript' },
+      },
+    ],
+    enforce: 'pre',
   });
 
   return config;
