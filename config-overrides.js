ed2760e8a9a8a704ec8b8d62993d2bcd30f81b38
Mateja Petrovic <mateja176@gmail.com>
2019-06-30 15:29:23 +0200

    refactor(config.overrides): removes react-hot-loader/webpack loader

--- config-overrides.js
+++ config-overrides.js
@@ -1,16 +1,9 @@
 /* eslint-disable immutable/no-mutation */
 
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
-const { produce } = require('immer');
 
 module.exports = (config, env) => {
   const hotConfig = rewireReactHotLoader(config, env);
 
-  return produce(hotConfig, newConfig => {
-    newConfig.module.rules.push({
-      test: /\.jsx?$/,
-      include: /node_modules/,
-      use: ['react-hot-loader/webpack'],
-    });
-  });
+  return hotConfig;
 };
