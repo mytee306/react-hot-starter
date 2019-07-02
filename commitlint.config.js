8730fbc4934d77f4b084991061bab2310596c233
Mateja Petrovic <mateja176@gmail.com>
2019-07-02 22:39:29 +0200

    feat(commitlint): disables scope-case and subject-case

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,2 +1,8 @@
 // eslint-disable-next-line immutable/no-mutation
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-case': [0],
+    'subject-case': [0],
+  },
+};
