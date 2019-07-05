6f8493ace9a17b6e0a2a48cee53d8b4c7135efaa
Mateja Petrovic <mateja176@gmail.com>
2019-07-05 23:40:22 +0200

    feat(epic): enables hot reloading
    issue: running subscriptions are not kept

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -2,11 +2,19 @@ import LogRocket from 'logrocket';
 import logger from 'redux-logger';
 import { createEpicMiddleware } from 'redux-observable';
 import { configureStore, getDefaultMiddleware } from 'redux-starter-kit';
+import { BehaviorSubject } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import { Module } from '../models/Module';
-import epic from './epic';
+import rootEpic from './epic';
 import reducer from './reducer';
+import { createGetCount } from './slices/count';
 
 const epicMiddleware = createEpicMiddleware();
+
+const epic$ = new BehaviorSubject(rootEpic);
+
+const hotReloadingEpic = (...args: Parameters<typeof rootEpic>) =>
+  epic$.pipe(switchMap(epic => epic(...args)));
 
 const middleware = [...getDefaultMiddleware(), epicMiddleware];
 
@@ -17,11 +25,17 @@ export default () => {
       middleware: middleware.concat(logger),
     });
 
-    epicMiddleware.run(epic);
+    epicMiddleware.run(hotReloadingEpic);
 
-    (module as Module).hot.accept('./reducer', () =>
-      store.replaceReducer(reducer),
-    );
+    (module as Module).hot.accept('./reducer', () => {
+      store.replaceReducer(reducer);
+    });
+
+    (module as Module).hot.accept('./epic', () => {
+      epic$.next(rootEpic);
+
+      store.dispatch(createGetCount());
+    });
 
     return store;
   } else {
@@ -30,7 +44,7 @@ export default () => {
       middleware: middleware.concat(LogRocket.reduxMiddleware()),
     });
 
-    epicMiddleware.run(epic);
+    epicMiddleware.run(rootEpic);
 
     return store;
   }
