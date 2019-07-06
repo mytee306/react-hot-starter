0be38db2af8b9f2915f3442d0d513b4d38a03432
Mateja Petrovic <mateja176@gmail.com>
2019-07-06 02:03:41 +0200

    refactor(Count/Decrement): replaces Formik with Slider

--- src/pages/Count/Decrement.tsx
+++ src/pages/Count/Decrement.tsx
@@ -1,72 +1,60 @@
-import { Divider, FormControl, TextField } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+import { Slider } from '@material-ui/lab';
 import Button from 'components/Button';
-import { Form, Formik } from 'formik';
-import { isEmpty } from 'ramda';
-import React, { FC, memo } from 'react';
+import React, { FC, useState } from 'react';
+import { Box } from 'rebass';
 import { CountState, CreateDecrementBy } from 'store/slices/count';
-import { number, object } from 'yup';
-
-export interface Values {
-  amount: CountState['value'];
-}
-
-const initialValues: Values = {
-  amount: 1,
-};
-
-export const schema = object().shape({
-  amount: number()
-    .min(-5)
-    .max(5)
-    .required(),
-});
 
 export interface DecrementProps {
   decrementBy: CreateDecrementBy;
   isLoading: CountState['isLoading'];
 }
 
-const Decrement: FC<DecrementProps> = ({ decrementBy, isLoading }) => (
-  <Formik
-    initialValues={initialValues}
-    onSubmit={values => {
-      const { amount } = values;
+const Decrement: FC<DecrementProps> = ({ decrementBy, isLoading }) => {
+  const [amount, setAmount] = useState(1);
 
-      decrementBy(amount);
-    }}
-    validationSchema={schema}
-  >
-    {({ values, errors, handleChange, handleBlur }) => (
-      <Form>
-        <FormControl>
-          <TextField
-            name="amount"
-            label="Decrement By Amount"
-            variant="outlined"
-            type="number"
-            value={values.amount}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            error={Boolean(errors.amount)}
-            helperText={errors.amount}
-          />
-        </FormControl>
-        <br />
-        <br />
-        <Divider />
-        <br />
-        <Button
-          color="secondary"
-          variant="contained"
-          type="submit"
-          isLoading={isLoading}
-          disabled={!isEmpty(errors)}
-        >
-          Decrement
-        </Button>
-      </Form>
-    )}
-  </Formik>
-);
+  return (
+    <form
+      onSubmit={e => {
+        e.preventDefault();
 
-export default memo(Decrement, () => true);
+        decrementBy(amount);
+      }}
+    >
+      <Box
+        style={{
+          display: 'grid',
+          gridAutoFlow: 'column',
+          alignItems: 'center',
+          gridGap: 10,
+        }}
+      >
+        <Typography>Decrement By Amount</Typography>
+        <Typography variant="title">{amount}</Typography>
+      </Box>
+      <br />
+      <br />
+      <Slider
+        min={1}
+        max={10}
+        value={amount}
+        onChange={(_, value) => setAmount(value)}
+        step={1}
+        style={{ marginLeft: 5 }}
+      />
+
+      <br />
+      <br />
+      <Button
+        color="secondary"
+        variant="contained"
+        type="submit"
+        isLoading={isLoading}
+      >
+        Decrement
+      </Button>
+    </form>
+  );
+};
+
+export default Decrement;
