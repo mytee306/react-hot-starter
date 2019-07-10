baacdbb21900ea837d2f9208037ef5b97e5fe82d
Mateja Petrovic <mateja176@gmail.com>
2019-07-11 01:12:55 +0200

    feat(HOCS): adds memo

--- src/components/HOCS/index.ts
+++ src/components/HOCS/index.ts
@@ -1,2 +1,8 @@
+import { isEqual } from 'lodash';
+import { memo as reactMemo } from 'react';
+
+export const memo = (Component: Parameters<typeof reactMemo>[0]) =>
+  reactMemo(Component, isEqual);
+
 export * from './withSpinner';
 export { default } from './withSpinner';
