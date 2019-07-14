41df7e2d46b69f613779e64c71293d9f26afae1d
Mateja Petrovic <mateja176@gmail.com>
2019-07-14 14:04:52 +0200

    feat(react-dnd): adds backend
    uses media query to switch between html5 and touch backends

--- src/App/Routes.tsx
+++ src/App/Routes.tsx
@@ -1,37 +1,47 @@
+import { useMediaQuery, WithTheme, withTheme } from '@material-ui/core';
 import { Switch } from 'components';
 import { absoluteRootPaths } from 'Layout/Nav';
 import { Count, Dashboard, Images, List, Signin, Store } from 'pages';
 import Canvas from 'pages/Canvas';
 import React, { FC } from 'react';
+import { DndProvider } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+import TouchBackend from 'react-dnd-touch-backend';
 import { Redirect, Route } from 'react-router-dom';
 
-export interface RoutesProps {
+export interface RoutesProps extends WithTheme {
   isSignedIn: boolean;
 }
 
-const Routes: FC<RoutesProps> = ({ isSignedIn }) => (
-  <Switch>
-    {isSignedIn ? null : <Route component={Signin} />}
-    <Route
-      path={absoluteRootPaths.signin}
-      render={() => <Redirect to={absoluteRootPaths.dashboard} />}
-    />
-    <Route exact path={absoluteRootPaths.dashboard} component={Dashboard} />
-    <Route path={absoluteRootPaths.count} component={Count} />
-    <Route path={absoluteRootPaths.images} component={Images} />
-    <Route path={absoluteRootPaths.store} component={Store} />
-    <Route path={absoluteRootPaths.list} component={List} />
-    <Route path={absoluteRootPaths.canvas} component={Canvas} />
-    {/* {absoluteRootPathnames
-      .filter(path => path === absoluteRootPaths.dashboard)
-      .map((path, i) => (
-        <Route
-          key={path}
-          path={path}
-          component={pages[textRootPathnames[i] as any]}
-        />
-      ))} */}
-  </Switch>
-);
+const Routes: FC<RoutesProps> = ({ isSignedIn, theme }) => {
+  const isMediumScreen = useMediaQuery(theme.breakpoints.up('md'));
 
-export default Routes;
+  return (
+    <DndProvider backend={isMediumScreen ? HTML5Backend : TouchBackend}>
+      <Switch>
+        {isSignedIn ? null : <Route component={Signin} />}
+        <Route
+          path={absoluteRootPaths.signin}
+          render={() => <Redirect to={absoluteRootPaths.dashboard} />}
+        />
+        <Route exact path={absoluteRootPaths.dashboard} component={Dashboard} />
+        <Route path={absoluteRootPaths.count} component={Count} />
+        <Route path={absoluteRootPaths.images} component={Images} />
+        <Route path={absoluteRootPaths.store} component={Store} />
+        <Route path={absoluteRootPaths.list} component={List} />
+        <Route path={absoluteRootPaths.canvas} component={Canvas} />
+        {/* {absoluteRootPathnames
+        .filter(path => path === absoluteRootPaths.dashboard)
+        .map((path, i) => (
+          <Route
+            key={path}
+            path={path}
+            component={pages[textRootPathnames[i] as any]}
+          />
+        ))} */}
+      </Switch>
+    </DndProvider>
+  );
+};
+
+export default withTheme(Routes);
