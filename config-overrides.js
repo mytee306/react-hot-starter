561c9c016b9117fcbbc81915ae07c6c590f9478f
Mateja Petrovic <mateja176@gmail.com>
2019-07-15 19:39:29 +0200

    feat(config.overrides): adds jest property

--- config-overrides.js
+++ config-overrides.js
@@ -1,16 +1,30 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable immutable/no-mutation */
 
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
-const { produce } = require('immer');
 
-module.exports = (config, env) => {
-  const hotConfig = rewireReactHotLoader(config, env);
+module.exports = {
+  webpack: (config, env) => {
+    const hotConfig = rewireReactHotLoader(config, env);
 
-  return produce(hotConfig, newConfig => {
-    newConfig.module.rules.push({
+    hotConfig.module.rules.push({
       test: /\.jsx?$/,
       include: /node_modules/,
       use: ['react-hot-loader/webpack'],
     });
-  });
+
+    return hotConfig;
+  },
+  jest: config => {
+    config.moduleNameMapper = {
+      '^dnd-core$': 'dnd-core/dist/cjs',
+      '^react-dnd$': 'react-dnd/dist/cjs',
+      '^react-dnd-html5-backend$': 'react-dnd-html5-backend/dist/cjs',
+      '^react-dnd-touch-backend$': 'react-dnd-touch-backend/dist/cjs',
+      '^react-dnd-test-backend$': 'react-dnd-test-backend/dist/cjs',
+      '^react-dnd-test-utils$': 'react-dnd-test-utils/dist/cjs',
+    };
+
+    return config;
+  },
 };
