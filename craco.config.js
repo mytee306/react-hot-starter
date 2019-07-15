12f9781332e5666219e62a77412609f44ce79fb6
Mateja Petrovic <mateja176@gmail.com>
2019-07-15 19:24:45 +0200

    feat: adds craco

---
+++ craco.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable immutable/no-mutation */
+
+module.exports = {
+  jest: {
+    configure: {
+      moduleNameMapper: {
+        '^dnd-core$': 'dnd-core/dist/cjs',
+        '^react-dnd$': 'react-dnd/dist/cjs',
+        '^react-dnd-html5-backend$': 'react-dnd-html5-backend/dist/cjs',
+        '^react-dnd-touch-backend$': 'react-dnd-touch-backend/dist/cjs',
+        '^react-dnd-test-backend$': 'react-dnd-test-backend/dist/cjs',
+        '^react-dnd-test-utils$': 'react-dnd-test-utils/dist/cjs',
+      },
+    },
+  },
+};
