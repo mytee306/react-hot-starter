3cd1828e09aa16e22bfc5f57bd50f4e28f2dd0cc
Mateja Petrovic <ma.petrovic@levi9.com>
2019-08-18 17:47:24 +0200

    feat(utils/hooks): adds useDeepSelector and useActions

--- src/utils/hooks/index.ts
+++ src/utils/hooks/index.ts
@@ -1,4 +1,7 @@
 import { useMediaQuery, useTheme } from '@material-ui/core';
+import { equals } from 'ramda';
+import { useDispatch, useSelector } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 export const useIsNotSmallScreen = () => {
   const theme = useTheme();
@@ -6,4 +9,16 @@ export const useIsNotSmallScreen = () => {
   const isNotSmallScreen = useMediaQuery(theme.breakpoints.up('md'));
 
   return isNotSmallScreen;
+};
+
+export const useDeepSelector = <S extends Parameters<typeof useSelector>[0]>(
+  selector: S,
+) => useSelector(selector, equals);
+
+export const useActions = <
+  Actions extends Parameters<typeof bindActionCreators>[0]
+>(actions: Actions) => {
+  const dispatch = useDispatch();
+
+  return bindActionCreators(actions, dispatch);
 };
