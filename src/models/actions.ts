e3e5686cea1198a77628531dc6dc3d47514c2506
Mateja Petrovic <ma.petrovic@levi9.com>
2019-10-28 16:43:29 +0100

    feat(models/actions): adds FilterActionByType

--- src/models/actions.ts
+++ src/models/actions.ts
@@ -3,3 +3,8 @@ import { Action, ActionCreator } from 'redux';
 export type SimpleAction = Action<string>;
 
 export type CreateSimpleAction = ActionCreator<SimpleAction>;
+
+export type FilterActionByType<
+  A extends SimpleAction,
+  ActionType extends string
+> = A extends Action<ActionType> ? A : never;
