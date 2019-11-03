165fe2766ed62959ed8c2bc9ffea2b77461a48d8
mateja <mateja176@gmail.com>
2019-11-03 22:15:32 +0100

    fix(package.json): remove redundant @types/storybook__addon-knobs
    upgrades node-sass
    replaces jest-dom with @testing-library/jest-dom

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,6 @@
+import '@testing-library/jest-dom/extend-expect';
 import '@testing-library/react/cleanup-after-each';
 import registerRequireContextHook from 'babel-plugin-require-context-hook/register';
-import 'jest-dom/extend-expect';
 import 'polyfill-object.fromentries';
 
 registerRequireContextHook();
