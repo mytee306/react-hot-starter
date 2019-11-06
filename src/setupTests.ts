8764c2a8ec736d94afce47f6855fe61bc929c809
mateja <mateja176@gmail.com>
2019-11-06 11:10:00 +0100

    test: adds dotenv support

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,5 +2,7 @@ import '@testing-library/react/cleanup-after-each';
 import registerRequireContextHook from 'babel-plugin-require-context-hook/register';
 import 'jest-dom/extend-expect';
 import 'polyfill-object.fromentries';
+import dotenv from 'dotenv';
 
+dotenv.config({ path: '../.env.test' });
 registerRequireContextHook();
