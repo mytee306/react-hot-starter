09fdc8f6f8294205ea31b0a0372fb058fc530b09
mateja <mateja176@gmail.com>
2019-11-06 13:44:53 +0100

    feat(CheckoutForm): adds purchase button which redirects to checkout

--- src/pages/CheckoutForm.tsx
+++ src/pages/CheckoutForm.tsx
@@ -1,6 +1,7 @@
 import { Typography } from '@material-ui/core';
 import { Button } from 'components';
 import React from 'react';
+import { RouteComponentProps } from 'react-router';
 import {
   CardElement,
   Elements,
@@ -8,34 +9,70 @@ import {
   ReactStripeElements,
 } from 'react-stripe-elements';
 import { Box } from 'rebass';
+import urlJoin from 'url-join';
 
-type StripeProps = ReactStripeElements.InjectedStripeProps;
+export interface CheckoutFormProps
+  extends ReactStripeElements.InjectedStripeProps,
+    RouteComponentProps {}
 
-export interface CheckoutFormProps extends StripeProps {}
+const CheckoutForm: React.FC<CheckoutFormProps> = ({
+  stripe,
+  location: { pathname },
+}) => {
+  const origin = process.env.REACT_APP_ORIGIN;
+  const redirectionURL = origin
+    ? urlJoin(origin, pathname)
+    : window.location.href;
 
-const CheckoutForm: React.FC<CheckoutFormProps> = ({ stripe }) => (
-  <Box className="checkout">
-    <Typography>Would you like to complete the purchase?</Typography>
-    <CardElement />
-    <Button
-      onClick={() => {
-        if (stripe) {
-          stripe
-            .createToken({ name: 'Name' })
-            .then(({ token }) => token)
-            .then(console.log); // eslint-disable-line no-console
-        }
-      }}
-    >
-      Purchase
-    </Button>
-  </Box>
-);
+  return (
+    <Box>
+      <Box mb={40}>
+        <Typography>Would you like to complete the purchase?</Typography>
+        <CardElement />
+        <Button
+          onClick={() => {
+            if (stripe) {
+              stripe
+                .createToken({ name: 'Name' })
+                .then(({ token }) => token)
+                .then(console.log); // eslint-disable-line no-console
+            }
+          }}
+        >
+          Purchase
+        </Button>
+      </Box>
+      <Button
+        variant="contained"
+        color="primary"
+        role="link"
+        onClick={() => {
+          if (stripe) {
+            (stripe as any)
+              .redirectToCheckout({
+                items: [{ sku: 'sku_G80fsca4Ygr259', quantity: 1 }],
+
+                successUrl: redirectionURL,
+                cancelUrl: redirectionURL,
+              })
+              .then((result: { error?: Error }) => {
+                if (result.error) {
+                  console.log(result.error); // eslint-disable-line no-console
+                }
+              });
+          }
+        }}
+      >
+        Purchase Trifle
+      </Button>
+    </Box>
+  );
+};
 
 const CheckoutFormElement = injectStripe(CheckoutForm);
 
-export default () => (
+export default (props: RouteComponentProps) => (
   <Elements>
-    <CheckoutFormElement />
+    <CheckoutFormElement {...props} />
   </Elements>
 );
