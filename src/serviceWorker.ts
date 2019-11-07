bcaaeaa7029f2ee9494d948cf91efed34e7602e9
mateja <mateja176@gmail.com>
2019-11-07 23:03:25 +0100

    feat(index): serviceWorker.register()

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -13,8 +13,6 @@
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read https://bit.ly/CRA-PWA
 
-const origin = process.env.REACT_APP_ORIGIN || '';
-
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
@@ -105,7 +103,7 @@ const checkValidServiceWorker = (swUrl: string, config?: Config) => {
 export const register = (config?: Config) => {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(origin, window.location.href);
+    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
@@ -114,7 +112,7 @@ export const register = (config?: Config) => {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${origin}/service-worker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
