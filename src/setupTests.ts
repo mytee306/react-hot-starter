53649670ebb12feff6d1b52e5369a899b2da37c8
mateja <mateja176@gmail.com>
2019-11-07 23:42:17 +0100

    docs: adds typedoc

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,4 +1,5 @@
 import '@testing-library/react/cleanup-after-each';
+// @ts-ignore
 import registerRequireContextHook from 'babel-plugin-require-context-hook/register';
 import dotenv from 'dotenv';
 import 'jest-dom/extend-expect';
