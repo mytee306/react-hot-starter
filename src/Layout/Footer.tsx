484126ba01813d28455372ff46f86f47974e3c02
mateja <mateja176@gmail.com>
2019-11-10 20:30:38 +0100

    test(App): fixes preval issue in preval code itself

--- src/Layout/Footer.tsx
+++ src/Layout/Footer.tsx
@@ -4,17 +4,12 @@ import preval from 'babel-plugin-preval/macro';
 import React from 'react';
 import { Flex } from 'rebass';
 
-const version =
-  process.env.NODE_ENV === 'test'
-    ? 1
-    : preval`
+const version = preval`
 const fs = require('fs');
 
-const json = fs.readFileSync('../../package.json', { encoding: 'UTF-8' });
-
-const package = JSON.parse(json);
-
-module.exports = package.version
+module.exports = process.env.NODE_ENV === 'test'
+  ? 0
+  : JSON.parse(fs.readFileSync('../../package.json', { encoding: 'UTF-8' })).version;
 
 `;
 
