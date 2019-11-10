ea28de07d6cf3ebde61801422d5b630d763466b6
mateja <mateja176@gmail.com>
2019-11-10 23:18:03 +0100

    fix(travis): splits up firebase config into separate env variables

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,14 @@
 const development = {
-  firebaseConfig: JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG || '{}'),
+  firebaseConfig: {
+    apiKey: process.env.REACT_APP_FIREBASE_CONFIG_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_CONFIG_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_CONFIG_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_CONFIG_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_CONFIG_STORAGE_BUCKET,
+    messagingSenderId:
+      process.env.REACT_APP_FIREBASE_CONFIG_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_CONFIG_APP_ID,
+  },
 
   appName: 'react hot starter',
 };
