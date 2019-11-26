8f0f6f217060563d5a46bf53b5b27f7e25bc044a
mateja <mateja176@gmail.com>
2019-11-26 20:06:32 +0100

    feat(index): unregisters serviceWorker is NODE_ENV is not prod

--- src/index.tsx
+++ src/index.tsx
@@ -13,4 +13,8 @@ render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+if (process.env.NODE_ENV === 'production') {
+  serviceWorker.register();
+} else {
+  serviceWorker.unregister();
+}
