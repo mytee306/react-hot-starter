b2c4168acaa22257de829e8fe5780fa6354ee1d2
mateja <mateja176@gmail.com>
2019-11-26 20:12:14 +0100

    feat(index): if root node has children hydrates instead of rendering

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,16 @@ import 'matchmedia-polyfill/matchMedia.addListener';
 import React from 'react';
 // // @ts-ignore
 // import { render } from 'react-snapshot';
-import { render } from 'react-dom';
+import { hydrate, render } from 'react-dom';
 import App from './App';
 import './index.scss';
 import * as serviceWorker from './serviceWorker';
 
-render(<App />, document.getElementById('root'));
+const root = document.getElementById('root');
+
+const renderOrHydrate = root && root.children.length ? hydrate : render;
+
+renderOrHydrate(<App />, root);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
