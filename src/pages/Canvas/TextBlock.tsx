8fcbfd9b70c4edeabea953048c9537b233af00db
mateja <mateja176@gmail.com>
2019-12-01 02:54:10 +0100

    fix(pages/Canvas/TextBlock): import

--- src/pages/Canvas/TextBlock.tsx
+++ src/pages/Canvas/TextBlock.tsx
@@ -1,16 +1,21 @@
 import { Card, CardContent } from '@material-ui/core';
-import { Editor, EditorProps } from 'components';
+import { Editor } from 'components';
 import { isEqual } from 'lodash';
-import { createDropText, DropTextPayload, WithDropResult } from 'models';
+import {
+  createDropText,
+  DropTextPayload,
+  EditorProps,
+  WithDropResult,
+} from 'models';
 import React from 'react';
 import { useDrag } from 'react-dnd';
 import { v4 } from 'uuid';
 
 interface TextBlockBaseProps extends DropTextPayload, WithDropResult {}
 
-const TextBlockBase: React.FC<
-  TextBlockBaseProps & { position: React.CSSProperties['position'] }
-> = ({ position, top, left, ...props }) => {
+const TextBlockBase: React.FC<TextBlockBaseProps & {
+  position: React.CSSProperties['position'];
+}> = ({ position, top, left, ...props }) => {
   const [stateProps, setStateProps] = React.useState(props);
 
   React.useEffect(() => {
@@ -49,8 +54,9 @@ const TextBlock: React.FC<TextBlockBaseProps> = props => (
 
 export default TextBlock;
 
-export const TextBlockTemplate: React.FC<
-  Pick<EditorProps, 'initialContent'>
-> = props => (
+export const TextBlockTemplate: React.FC<Pick<
+  EditorProps,
+  'initialContent'
+>> = props => (
   <TextBlockBase {...props} position="static" top={0} left={0} id={v4()} />
 );
