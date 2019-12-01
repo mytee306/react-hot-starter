8035484f8e0077795f4a445568b7c9f6d7214ec3
mateja <mateja176@gmail.com>
2019-12-01 12:54:51 +0100

    feat(semantic-release): installs plugins

--- release.config.js
+++ release.config.js
@@ -3,8 +3,10 @@
 module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/github',
     '@semantic-release/changelog',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/git',
+    '@semantic-release/github',
+    '@semantic-release/npm',
   ],
 };
